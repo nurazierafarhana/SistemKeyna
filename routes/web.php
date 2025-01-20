f9f0f28b3b8a817a34733b6610f1e799fde9841d
nurazierafarhana <d097812@siswa.upsi.edu.my>
2025-01-20 20:01:49 +0800

    landing page

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,15 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LandingPageController;
+
+Route::get('/', [LandingPageController::class, 'index']);
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
